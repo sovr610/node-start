d3256dfe167088fac6bfbb3b7c76a8bf1d732e53
Parker Bidigare <work.simpleintelligence@gmail.com>
2022-04-05 13:55:35 -0400

    added args

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,40 @@
+import { doSpawn } from 'builder-util';
+
 const arg = require('arg');
+const fs = require('fs');
+const path = require('path')
+const shell = require('shelljs');
+
+function buildProject(type, lang, dir, name){
+    fs.mkdirSync(path.join(dir,name));
+    var inFolderDir = path.join(dir,name);
+    shell.cd(inFolderDir);
+    shell.exec('npm init');
+
+    fs.writeFileSync('index.js');
+    var gulp = `
+        const gulp = require('gulp');
+        const {src,dest} = require('gulp');
+        const sourcemaps = require('gulp-sourcemaps');
+        const concat = require('gulp-concat');
+        const css = require('gulp-clean-css');
+        
+    `
+    
+}
 
 export function cli(args) {
   function parseArgumentsForDataConfig(rawArgs) {
     const args = arg(
       {
         '--help': Boolean,
-        '--init': Boolean,
+        '--init': String,
+        '--lang': String,
+        '--name': String,
+        '--nopack': Boolean,
+        '-np': '--nopack',
+        '-n': '--name',
+        '-l': '--lang',
         '-h': '--help',
         '-i': '--init',
       },
@@ -15,7 +44,9 @@ export function cli(args) {
     );
     return {
       help: args['--help'] || false,
-      init: args['--init'] || false,
+      init: args['--init'] || 'empty',
+      lang: args['--lang'] || 'JS',
+      nopack: args['--nopack'] || false
     };
   }
 
@@ -23,8 +54,15 @@ export function cli(args) {
   if (agrc.help) {
     console.log('pad-ml attributes:');
     console.log(
-      '--init, -i: create base enviornment to create the .net library class projects and ML.net models'
+      '--init, -i: Create a initial nodejs project, default is empty project with no template, project types: console, express-web, neural-network, webassembly, empty',
+      '--lang, -l: set the language used on node: JS -> as Javascript amd TS -> as typescript'
     );
   }
+
+  const dir = process.cwd();
+  if(args.init == 'empty'){
+      
+  }
+
   console.log(args);
 }
